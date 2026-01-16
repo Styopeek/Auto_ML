c17ebf627a04bf1f82804addb9096bb73c8166d5
Stepa <styopeek@gmail.com>
2026-01-16 11:44:08 +0500

    Delete Auto_ML directory

--- Auto_ML/data/change.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv("data/current.csv")
-# Искусственный drift: увеличим Fare
-df["Fare"] = df["Fare"] / 10
-df.to_csv("data/current.csv", index=False)
-print("current.csv обновлён с drift")
